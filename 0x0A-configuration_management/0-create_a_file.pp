d13925e156dde2d65ddce21a4a50a8331639d71d
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-09-22 14:23:02 -0700

    Fix code in 0-create_a_file.pp file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 # Create a file in /tmp using Puppet
 
-file { '/tmp/school':
-  ensure => file,
-  mode   => '0744',
-  owner  => 'www-data',
-  group  => 'www-data',
+file { 'school':
+  ensure  => 'file',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
