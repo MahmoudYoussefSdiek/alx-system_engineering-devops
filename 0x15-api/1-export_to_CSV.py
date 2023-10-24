fe3782b5b9e71399011386525d46691f2efe2817
mahmoudyoussefsdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-10-23 17:51:03 -0700

    Update code in 1-export_to_CSV.py file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -21,9 +21,17 @@ if __name__ == '__main__':
     user_data = user_response.json()
     todo_data = todo_response.json()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, user_data.get("username"),
-             item.get("completed"), item.get("title")]
-         ) for item in todo_data]
+    csv_filename = '{}.csv'.format(user_id)
+    with open(csv_filename, mode='w') as csv_file:
+        fieldnames = [
+            'USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for task in todo_data:
+            writer.writerow({
+                'USER_ID': user_id,
+                'USERNAME': user_data.get("username"),
+                'TASK_COMPLETED_STATUS': task.get("completed"),
+                'TASK_TITLE': task.get("title")
+            })
