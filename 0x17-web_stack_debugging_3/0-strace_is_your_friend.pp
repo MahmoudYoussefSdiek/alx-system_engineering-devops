c53d019969e9e04c8921a12aec3adefacaa7b28f
Mahmoud Youssef <111357017+MahmoudYoussefSdiek@users.noreply.github.com>
2023-11-09 02:10:14 +0200

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,21 @@
+# Create a new configuration file for Apache
+file { '/etc/apache2/conf-available/debug.conf':
+  content => "LogLevel debug\n",
+}
+
+# Enable the new configuration file
+exec { 'enable-debug-conf':
+  command => 'a2enconf debug',
+  path    => '/usr/sbin:/usr/bin:/sbin:/bin',
+  creates => '/etc/apache2/conf-enabled/debug.conf',
+}
+
+# Restart Apache to apply the new configuration
+service { 'apache2':
+  ensure    => 'running',
+  subscribe => File['/etc/apache2/conf-available/debug.conf'],
+}
+
 # Trace the Apache process with strace
 exec { 'trace-apache':
   command     => 'strace -f -o /tmp/apache.strace -p $(pgrep apache2)',
