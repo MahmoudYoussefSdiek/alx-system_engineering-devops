638b6c4ffe7d82503a98b205507ca4edc2d6ca80
Mahmoud Youssef <111357017+MahmoudYoussefSdiek@users.noreply.github.com>
2023-11-17 03:32:47 +0200

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,20 +1,12 @@
 # 0-the_sky_is_the_limit_not.pp
 
-# Install Nginx package
-package { 'nginx':
-  ensure => installed,
-}
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
-# Configure Nginx server
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  content => template('your_template_file.erb'),
-  notify  => Service['nginx'],
-}
-
-# Restart Nginx service
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/nginx.conf'],
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
